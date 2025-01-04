713794487035d079241261267069ef59a98e4733
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-01-04 17:28:31 -0300

    ex008
     priemeiro arquivo JS

---
+++ aula10/ex008.js
@@ -0,0 +1,16 @@
+var speed = 60
+console.log(`a velocidade do seu carro é ${speed}km/h`)
+if (speed > 60) {
+    console.log("Voce foi multado!")
+}
+    
+
+console.log("dirija sempre usando cinto de segurança!")
+
+var pais = "EUA"
+console.log (`Voce esta morando em: ${pais}`)
+if (pais == "brasil") {
+    console.log("voce é brasileiro(a)!")
+} else {
+    console.log("Voce nao é brasileiro(a)...")
+}
