aa9b1cf29a802c4a2ff331433ca3400df4b9ecc5
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-01-30 20:09:23 -0300

    AULA 15
    praticando arrays

---
+++ aula15/teste.js
@@ -0,0 +1,45 @@
+/*CRIANDO UMA ARRAY*/ 
+
+let frutas = ["maca", "abacaxi" , "banana" , "tomate" ];
+
+/*MOSTRANDO O ULTIMO ELEMENTO DO ARRAY
+console.log(frutas[3]);
+*/ 
+
+/*MNOSTRANDO O ARRAY EM ORDEM ALFABETICA (ordem crescesnte em caso de dados do tipo NUMBER) 
+frutas.sort();
+console.log(frutas);
+*/
+
+/*CRIANDO UM ARRAY MULDIDIMENSIONAL */
+let valor = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8 ,9],
+]
+
+
+/*MOSTRANDO O ELEMENTO DA LINHA 1 COLUNA 2 
+console.log(valor[0][1]);
+*/
+
+/*ADICIONANDO UMA STRING NA LINHA 2 COLUNA 
+valor[1][0] = "STRING";
+console.log(valor);
+1*/ 
+
+/*apagando a ultimo elemento do array (geral)
+valor.pop();
+console.log(valor);
+*/ 
+
+/*Apagando somente um unico elemento dentro de um array multimensional, apontando sua linha e sua coluna
+valor[1].splice(2);
+console.log(valor);
+*/ 
+
+/*mostrando a quantidade de elemetos que existem no meu array multidimensional
+console.log(valor.length);
+*/ 
+
+
