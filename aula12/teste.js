09e3e763c5bead35bd9bd21a0d01c00d256837a4
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-01-31 18:12:33 -0300

    aula 16

---
+++ aula12/teste.js
@@ -0,0 +1,11 @@
+
+
+function saudacao() {
+    if (nome === ``) {
+        console.log(`olá!`) 
+    } else {
+        console.log(`Olá, ${nome}!`)
+    }
+}
+let nome = `thiago`;
+saudacao(nome)
---
+++ aula16/funcao01.js
@@ -0,0 +1,12 @@
+var n = 6
+
+function parimp(n) {
+    if (n%2==0) {
+        return "par"
+    } else {
+        return "impar"
+    }
+}
+
+let res = parimp(n);
+console.log(res);
