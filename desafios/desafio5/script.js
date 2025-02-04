fe1a897499321cc2b0abbdea496fc30b4c6e54e5
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-02-04 14:44:14 -0300

    ultima aula do curso javascreipt basico

--- desafios/desafio5/script.js
+++ desafios/desafio5/script.js
@@ -1,24 +1,54 @@
 let numeros = []
+let res = document.querySelector(`.res`);
 
 function adicionar() {
-    let valor = document.getElementById(`valor`).value.trim();
-    let numero = Number(valor);
+    let valorDigitado = document.getElementById(`valor`).value.trim();
+    let numero = Number(valorDigitado);
     
-    if (numero && valor !== ``) {
+    if (numero && valorDigitado !== ``) {
+        numeros.push(numero);
         let lista = document.querySelector(`.lista-num`);
         let item = document.createElement(`li`);
         item.textContent = numero;
         lista.appendChild(item);
+        res.innerHTML = ``
     } else {
         alert(`DIGITE UM NUMERO VÁLIDO!`);
     }
+    valor.value = `` 
+    valor.focus()
 }
 
 function finalizar() {
-    let final = document.querySelector(`.res`);
-    let paragrafo = document.createElement(`p`);
-    paragrafo.textContent = `a ordem crescente dos numeros da sua lista é:${numeros.sort((a, b) => a - b)}`;
-    final.appendChild(paragrafo);
+    if (numeros.length == 0) {
+        alert(`Adicione um numero antes de finalizar`);
+    } else {
+    let maior = numeros[0];
+    let menor = numeros[0];
+    let soma = 0
+    let media = 0
+
+
+    for(let pos in numeros) {
+        soma += numeros[pos]
+        if(numeros[pos] > maior)
+            maior = numeros[pos]
+        if (numeros[pos] < menor)
+            menor = numeros[pos]
+    }
+    let total = numeros.length
+    media = soma/total;
+
+    res.innerHTML = ``
+    res.innerHTML += `<p>Ao todo temos ${total} numeros cadastrados.</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    res.innerHTML += `<p>A soma total do svalores informados é: ${soma}</p>`
+    res.innerHTML += `<p>A média dos valores informados é: ${media}</p>`
+
+    }
 }
+
+
 
 
